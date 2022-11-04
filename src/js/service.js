8445d1608eb8fd61cd98fe7d50f5a419e09a9a40
Reqvite <reqvite2233@gmail.com>
2022-11-04 17:38:12 +0200

    fix func

--- src/js/service.js
+++ src/js/service.js
@@ -4,7 +4,7 @@ export { fetchFilmDetails };
 
 const API_KEY = 'e145377b3a98d62607e7dc90339d279b';
 
-  fetchFilmDetails = () => {
+  const fetchFilmDetails = () => {
     return  axios({
     method: 'get',
     url: `https://api.themoviedb.org/3/movie/${766475}?${API_KEY}b&language=en-US`,
