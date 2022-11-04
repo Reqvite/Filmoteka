df4aa5f0d4215ead5beab1bd4368852cc66a1156
Reqvite <reqvite2233@gmail.com>
2022-11-04 19:15:42 +0200

    add header

--- src/js/openFilmModal.js
+++ src/js/openFilmModal.js
@@ -1,9 +1,10 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container-main')
 const btn = document.querySelector('.test-modal');
-const modal = document.querySelector('.film-modal')
-const modalClose = document.querySelector('.modal-icon-cross')
+const modal = document.querySelector('.film-modal');
+const modalClose = document.querySelector('.modal-icon-cross');
 
 container.addEventListener('click', (e) => {
+    console.log(1)
     if (e.target.getAttribute('class')?.includes('test-modal')) {
         modal.classList.remove('is-hidden') 
         modalClose.addEventListener('click', e => {
