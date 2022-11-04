4e105608e5d42beb3bcb9db1a33615b11007e546
Reqvite <reqvite2233@gmail.com>
2022-11-04 19:33:49 +0200

    fix

--- src/js/addFilmsAndrenderGalery.js
+++ src/js/addFilmsAndrenderGalery.js
@@ -1,55 +1,55 @@
-import FilmsApiServer from './fimlsApiServer';
+// import FilmsApiServer from './fimlsApiServer';
 
-const filmsApiServer = new FilmsApiServer();
-const refs = {
-  form: document.querySelector('.search-form'), // .search-form - селектор форми в якій знаїодиться інпут - поле пошуку
-  gallery: document.querySelector('.gallery'), // .gallery- селектор контейнема в який буде рендеритися дані з бекенду
-};
+// const filmsApiServer = new FilmsApiServer();
+// const refs = {
+//   form: document.querySelector('.search-form'), // .search-form - селектор форми в якій знаїодиться інпут - поле пошуку
+//   gallery: document.querySelector('.gallery'), // .gallery- селектор контейнема в який буде рендеритися дані з бекенду
+// };
 
-refs.form.addEventListener('submit', onSubmitForm);
+// refs.form.addEventListener('submit', onSubmitForm);
 
-function onSubmitForm(e) {
-  e.preventDefault();
-  clearGalleryContainer();
-  filmsApiServer.resetPage();
+// function onSubmitForm(e) {
+//   e.preventDefault();
+//   clearGalleryContainer();
+//   filmsApiServer.resetPage();
 
-  filmsApiServer.query = e.currentTarget.searchQuery.value.trim();
+//   filmsApiServer.query = e.currentTarget.searchQuery.value.trim();
 
-  if (filmsApiServer.query === '') {
-    Notiflix.Notify.warning(
-      'Please enter your search query'
-    );
+//   if (filmsApiServer.query === '') {
+//     Notiflix.Notify.warning(
+//       'Please enter your search query'
+//     );
 
-    return;
-  }
+//     return;
+//   }
 
-  addFilmsAndUpdateUI();
-}
+//   addFilmsAndUpdateUI();
+// }
 
-async function addFilmsAndUpdateUI() {
-  try {
-	const { results } = await filmsApiServer.fetchFilms();
-    renderGalleryList(results);
-    // filmsApiServer.increasePage();
+// async function addFilmsAndUpdateUI() {
+//   try {
+// 	const { results } = await filmsApiServer.fetchFilms();
+//     renderGalleryList(results);
+//     // filmsApiServer.increasePage();
 
-  } catch (err) {
-    onFetchError(err);
-  }
-}
+//   } catch (err) {
+//     onFetchError(err);
+//   }
+// }
 
-function renderGalleryList(data) {
+// function renderGalleryList(data) {
 
-  if (data.length === 0) {
-    Notiflix.Notify.failure(
-      'Search result not successful. Enter the correct movie name and try again'
-    );
-    clearGalleryContainer();
-    return;
-  }
+//   if (data.length === 0) {
+//     Notiflix.Notify.failure(
+//       'Search result not successful. Enter the correct movie name and try again'
+//     );
+//     clearGalleryContainer();
+//     return;
+//   }
 
-  refs.gallery.insertAdjacentHTML('beforeend', fotoCardsTpl(data)); //fotoCardsTpl(data) - функція яка рендерить HTML сторінку, data - масив обєктів
-}
+//   refs.gallery.insertAdjacentHTML('beforeend', fotoCardsTpl(data)); //fotoCardsTpl(data) - функція яка рендерить HTML сторінку, data - масив обєктів
+// }
 
-function clearGalleryContainer() {
-  refs.gallery.innerHTML = '';
-}
+// function clearGalleryContainer() {
+//   refs.gallery.innerHTML = '';
+// }
--- src/js/fimlsApiServer.js
+++ src/js/fimlsApiServer.js
@@ -1,42 +1,42 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
-const API_KEY = 'e145377b3a98d62607e7dc90339d279b';
+// const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+// const API_KEY = 'e145377b3a98d62607e7dc90339d279b';
 
-export default class FilmsApiServer {
-  constructor() {
-    (this.searchQuery = 'man'), (this.page = 1), (this.perPage = 20);
-  }
+// export default class FilmsApiServer {
+//   constructor() {
+//     (this.searchQuery = 'man'), (this.page = 1), (this.perPage = 20);
+//   }
 
-  async fetchFilms() {
-    const options = {
-      params: {
-        api_key: API_KEY,
-        query: `${this.searchQuery}`,
-        page: `${this.page}`,
-      },
-    };
-    const data = await axios.get(`${BASE_URL}`, options);
-    return data;
-  }
+//   async fetchFilms() {
+//     const options = {
+//       params: {
+//         api_key: API_KEY,
+//         query: `${this.searchQuery}`,
+//         page: `${this.page}`,
+//       },
+//     };
+//     const data = await axios.get(`${BASE_URL}`, options);
+//     return data;
+//   }
 
-  get query() {
-    return this.searchQuery;
-  }
+//   get query() {
+//     return this.searchQuery;
+//   }
 
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
 
-  increasePage() {
-    this.page += 1;
-  }
+//   increasePage() {
+//     this.page += 1;
+//   }
 
-  decreasePage() {
-    this.page -= 1;
-  }
+//   decreasePage() {
+//     this.page -= 1;
+//   }
 
-  resetPage() {
-    this.page = 1;
-  }
-}
+//   resetPage() {
+//     this.page = 1;
+//   }
+// }
