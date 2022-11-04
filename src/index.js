52b10113d9360f6d8ff8f3d2dbc3ee1a2e234656
Reqvite <reqvite2233@gmail.com>
2022-11-04 12:56:30 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './sass/index.scss';
 
+import './js/service';
+
+import './js/markups/filmDetailMarkup'
---
+++ src/js/markups/filmDetailMarkup.js
@@ -0,0 +1,43 @@
+import { fetchFilmDetails } from "../service";
+
+const modal = document.querySelector('.modal');
+
+const createFilmDetailsMarkup = async () => {
+    const resp = await fetchFilmDetails();
+    const { poster_path, original_title, vote_average, vote_count, popularity, genres, overview } = resp.data;  
+
+    const markup = `<div class="film-details-wrapper">
+  <div><img class="modal-img" src="https://image.tmdb.org/t/p/original/${poster_path}" alt="" /></div>
+  <div class="film-details">
+    <h2 class="film-details__main-title">${original_title}</h2>
+    <ul class="details-list list">
+      <li class="details-list__item">
+        <p class="details-list_title">Vote / Votes</p>
+        <span class="details-list__information">${vote_average} / ${vote_count}</span>
+      </li>
+      <li class="details-list__item">
+        <p class="details-list_title">Popularity</p>
+        <span class="details-list__information">${popularity}</span>
+      </li>
+      <li class="details-list__item">
+        <p class="details-list_title">Original Title</p>
+        <span class="details-list__information">${original_title}</span>
+      </li>
+      <li class="details-list__item">
+        <p class="details-list_title">Genre</p>
+        <span class="details-list__information">${genres.map(el => {
+            return el.name
+        })}</span>
+      </li>
+    </ul>
+    <h3 class="film-details__secondary-title">About</h3>
+    <p class="film-details__about">${overview}</p>
+  </div>
+</div>`
+    
+    modal.insertAdjacentHTML("beforeend", markup) 
+}
+
+createFilmDetailsMarkup()
+ 
+ 
