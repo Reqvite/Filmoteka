ee093f8685c57977fd28af3e8fc1f9f976e1eb42
Reqvite <reqvite2233@gmail.com>
2022-11-06 01:23:02 +0200

    upd firebase

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, set, ref, enableLogging, update  } from "firebase/database";
+import { getDatabase, set, ref, enableLogging, update,child, get } from "firebase/database";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,14 +21,32 @@ const loginBtn = document.querySelector('.login-Btn');
 const logOut = document.querySelector('.login-Out');
 
 
-const nameInput = document.querySelector('.name-input')
+const nameInput = document.querySelector('.name-input');
+const submitSignBtn = document.querySelector('.submit-signUp-btn');
+const submitLoginBtn = document.querySelector('.submit-login-btn');
+
 
 const modal = document.querySelector('.backdrop-form');
 
-console.log(modal)
+const modalFormBtnClose = document.querySelector('.modal-form-btn');
+
+const myLibraryJs = document.querySelector('.my-library-js')
+
+myLibraryJs.style.display = 'none'
+
+modalFormBtnClose.addEventListener('click', e => {
+    modal.classList.add('form-hidden');
+    formRegistr.removeEventListener('submit', signUpUser);
+    formRegistr.removeEventListener('submit', logInUser); 
+    submitSignBtn.style.display = 'none';
+    submitLoginBtn.style.display = 'none';
+})
+
+
 signinBtn.addEventListener('click', e => {
     modal.classList.remove('form-hidden');
     formRegistr.addEventListener('submit', signUpUser) 
+    submitSignBtn.style.display = 'block';
 })
 
 // Initialize Firebase
@@ -36,9 +54,11 @@ const app = initializeApp(firebaseConfig);
   const database = getDatabase(app);
   const auth = getAuth();
 
+
+   
+
+  
 const formRegistr = document.querySelector('.registr')
-
-
 
 function signUpUser(e) {
      e.preventDefault()
@@ -46,7 +66,6 @@ function signUpUser(e) {
     const email = formRegistr.elements[1].value
     const password = formRegistr.elements[2].value
 
-    e.target.reset()
     modal.classList.add('form-hidden');
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -57,24 +76,31 @@ function signUpUser(e) {
                 username: username,
                 email: email
             })
+
             modal.classList.add('form-hidden');
             signinBtn.style.display = 'none';
             loginBtn.style.display = 'none';
-            logOut.classList.remove('is-hidden')
+            logOut.classList.remove('is-hidden');
+            myLibraryJs.style.display = 'block';
+            submitSignBtn.style.display = 'none';
+            formRegistr.removeEventListener('submit', signUpUser);
             alert('created')
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // ..
+             formRegistr.removeEventListener('submit', signUpUser);
             alert(errorMessage)
-        });
+        })
+        e.target.reset()
 }
 
 loginBtn.addEventListener('click', e => {
     modal.classList.remove('form-hidden');
     nameInput.style.display = 'none';
-    formRegistr.addEventListener('submit', logInUser) 
+    submitSignBtn.style.display = 'none';
+    submitLoginBtn.style.display = 'block';
+    formRegistr.addEventListener('submit', logInUser); 
 })
 
 function logInUser(e) {
@@ -90,17 +116,22 @@ function logInUser(e) {
     // ...
        const dt = new Date();
          update(ref(database, 'users/' + user.uid),{
-          last_login: dt,
+             last_login: dt,
+             localData: 'data'
          })
             modal.classList.add('form-hidden');
             signinBtn.style.display = 'none';
       loginBtn.style.display = 'none';
-      logOut.classList.remove('is-hidden')
+      logOut.classList.remove('is-hidden');
+      myLibraryJs.style.display = 'block';
+      submitLoginBtn.style.display = 'none';
+      formRegistr.addEventListener('submit', logInUser);
       alert('login')
   })
   .catch((error) => {
     const errorCode = error.code;
       const errorMessage = error.message;
+    formRegistr.removeEventListener('submit', signUpUser);
       alert(errorMessage)
   });   
 }
@@ -111,7 +142,18 @@ onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
+      const uid = user.uid;
+      
+        const dbRef = ref(getDatabase());
+get(child(dbRef, `users/${uid}`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
     // ...
   } else {
     // User is signed out
@@ -133,7 +175,6 @@ logOut.addEventListener('click', e => {
       alert(errorMessage)
 });
 })
-
 
 
 
