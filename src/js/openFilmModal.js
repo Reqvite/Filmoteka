31b52d8fe742a0b59a5069d0f3478b2d4d9bc0a5
Reqvite <reqvite2233@gmail.com>
2022-11-05 15:44:12 +0200

    add favicoin,fix modal

--- src/js/openFilmModal.js
+++ src/js/openFilmModal.js
@@ -8,21 +8,21 @@ const body = document.querySelector('body')
 
 const openModal = async e => {
     if (e.target.getAttribute('class')?.includes('card-film')) {
-        const resp = await fetchFilmDetails(e.target.closest('.card-film').dataset.id)
-        createFilmDetailsMarkup(resp)
-        modal.classList.remove('hidden') 
-        body.style.overflow = 'hidden'
-        modalClose.addEventListener('click', closeModal)
+        const resp = await fetchFilmDetails(e.target.closest('.card-film').dataset.id);
+        createFilmDetailsMarkup(resp);
+        modal.classList.remove('hidden');
+        body.style.overflow = 'hidden';
+        modalClose.addEventListener('click', closeModal);
     }
 }
 
 container.addEventListener('click', openModal)
 
 const  closeModal = e =>{
-    const modalContainer = document.querySelector('.film-details-wrapper')
+    const modalContainer = document.querySelector('.film-details-wrapper');
     modal.classList.add('hidden');
     modalClose.removeEventListener('click', closeModal);
-    body.style.overflow = 'scroll'
+    body.style.overflow = 'scroll';
     modalContainer.remove();
 }
 
