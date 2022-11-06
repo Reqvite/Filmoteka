927fd0761f1478eef068b0e23fc24d848442d08c
VladyslavLysenko <Dodog12362@gmail.com>
2022-11-06 10:08:29 +0200

    commented pagination

--- src/js/collection.js
+++ src/js/collection.js
@@ -24,17 +24,17 @@ function fetchMovies(page) {
   fetchPopularMovies(page).then(response => {
     console.log(response.data.results);
   const render = renderMarkUp(response.data.results, genreCollection);
-  const renderedPagination = renderPagination(Number(response.data.page), Number(response.data.total_pages))
+  // const renderedPagination = renderPagination(Number(response.data.page), Number(response.data.total_pages))
     collection.innerHTML = render;
-    pagination.innerHTML = renderedPagination;
+    // pagination.innerHTML = renderedPagination;
 
 })
 
 }
 
 // pagination by VladLysenko
-pagination.addEventListener("click", (e) => {
-  e.preventDefault()
-  fetchMovies(e.target.dataset.page)
-})
+// pagination.addEventListener("click", (e) => {
+//   e.preventDefault()
+//   fetchMovies(e.target.dataset.page)
+// })
 fetchMovies(page)
--- src/js/collectionRender.js
+++ src/js/collectionRender.js
@@ -50,70 +50,70 @@ export function renderMarkUp(arrMovies, genreCollection) {
 
 
 // pagination var Vlad Lysenko
-export function renderPagination(currentPage, total_pages) {
-    let paginationArr = [];
-    let paginationLinks = "";
-    // var 1 
-    if (currentPage <= 3) {
-        if (currentPage > 2) {
-          paginationArr.push(currentPage - 2 );  
-        }
-        if (currentPage > 1) {
-          paginationArr.push(currentPage - 1 );  
-        }
-        paginationArr.push(currentPage);  
-        paginationArr.push(currentPage + 1);
-        paginationArr.push(currentPage + 2);
-        if (currentPage < 3) {
-            paginationArr.push(currentPage + 3);  
-        }
-        paginationArr.push("...");
-        paginationArr.push(total_pages);
-    // var 2
-    } else if (currentPage > 3 && (total_pages - currentPage) > 3) {
-        paginationArr.push(1);
-        paginationArr.push("...");
-        if (currentPage > 4) {
-            paginationArr.push(currentPage - 2);
-        }
-        paginationArr.push(currentPage - 1);
-        paginationArr.push(currentPage);
-        paginationArr.push(currentPage + 1);
-        paginationArr.push(currentPage + 2);
-        paginationArr.push("...");
-        paginationArr.push(total_pages);
-    } else {
-        paginationArr.push(1);
-        paginationArr.push("...");
-        paginationArr.push(currentPage - 2);
-        paginationArr.push(currentPage - 1);
-        paginationArr.push(currentPage);
-        if ((total_pages - currentPage) >= 3) {
-            paginationArr.push(currentPage + 1);
-            paginationArr.push(currentPage + 2);           
-        }
-        if (currentPage !== total_pages ) {
-            paginationArr.push(total_pages);       
-        }
-    }
+// export function renderPagination(currentPage, total_pages) {
+//     let paginationArr = [];
+//     let paginationLinks = "";
+//     // var 1 
+//     if (currentPage <= 3) {
+//         if (currentPage > 2) {
+//           paginationArr.push(currentPage - 2 );  
+//         }
+//         if (currentPage > 1) {
+//           paginationArr.push(currentPage - 1 );  
+//         }
+//         paginationArr.push(currentPage);  
+//         paginationArr.push(currentPage + 1);
+//         paginationArr.push(currentPage + 2);
+//         if (currentPage < 3) {
+//             paginationArr.push(currentPage + 3);  
+//         }
+//         paginationArr.push("...");
+//         paginationArr.push(total_pages);
+//     // var 2
+//     } else if (currentPage > 3 && (total_pages - currentPage) > 3) {
+//         paginationArr.push(1);
+//         paginationArr.push("...");
+//         if (currentPage > 4) {
+//             paginationArr.push(currentPage - 2);
+//         }
+//         paginationArr.push(currentPage - 1);
+//         paginationArr.push(currentPage);
+//         paginationArr.push(currentPage + 1);
+//         paginationArr.push(currentPage + 2);
+//         paginationArr.push("...");
+//         paginationArr.push(total_pages);
+//     } else {
+//         paginationArr.push(1);
+//         paginationArr.push("...");
+//         paginationArr.push(currentPage - 2);
+//         paginationArr.push(currentPage - 1);
+//         paginationArr.push(currentPage);
+//         if ((total_pages - currentPage) >= 3) {
+//             paginationArr.push(currentPage + 1);
+//             paginationArr.push(currentPage + 2);           
+//         }
+//         if (currentPage !== total_pages ) {
+//             paginationArr.push(total_pages);       
+//         }
+//     }
 
-    paginationArr.forEach(pageNumber => {
-        let activeClass = pageNumber === currentPage ? "active" : "";
+//     paginationArr.forEach(pageNumber => {
+//         let activeClass = pageNumber === currentPage ? "active" : "";
      
 
-        paginationLinks = paginationLinks + `<li class="pagination__item"><a data-page="${pageNumber}" href="${buildUrl(pageNumber)}" class="pagination__link ${activeClass} ">${pageNumber}</a></li>`
+//         paginationLinks = paginationLinks + `<li class="pagination__item"><a data-page="${pageNumber}" href="${buildUrl(pageNumber)}" class="pagination__link ${activeClass} ">${pageNumber}</a></li>`
         
-    });
+//     });
 
-    return `
-    <ul class="pagination__list">
-        <li class="pagination__item"><a href="" class="pagination__link">&lt;</a></li>
-        ${paginationLinks}
-        <li class="pagination__item"><a href="" class="pagination__link">&gt;</a></li>
-    </ul>
-    `
+//     return `
+//     <ul class="pagination__list">
+//         <li class="pagination__item"><a href="" class="pagination__link">&lt;</a></li>
+//         ${paginationLinks}
+//         <li class="pagination__item"><a href="" class="pagination__link">&gt;</a></li>
+//     </ul>
+//     `
     
-}
+// }
 
 
 
