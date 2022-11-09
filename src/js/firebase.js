6e4da3f01cf3b01e3ea2ac05083bb7899c0ca1d6
Yurii PErekrestnyi <jodlei1618@gmail.com>
2022-11-09 14:24:31 +0200

    check!!!

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,23 +1,35 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-import { initializeApp } from "firebase/app";
-import { getDatabase, set, ref, enableLogging, update,child, get } from "firebase/database";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import {
+  getDatabase,
+  set,
+  ref,
+  enableLogging,
+  update,
+  child,
+  get,
+} from 'firebase/database';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
 
 import { switchToHome } from './changeHeaderPageHome-Mylibrary';
-import { fetchTrendingFilms } from './collection'
+import { fetchTrendingFilms } from './collection';
 import { refs } from './refs/refs';
 
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAmrcw3LWh5vdrPE40gh2Uggxq3EG96Lys",
-  authDomain: "film-library-registration.firebaseapp.com",
-  databaseURL: "https://film-library-registration-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "film-library-registration",
-  storageBucket: "film-library-registration.appspot.com",
-  messagingSenderId: "394290136676",
-  appId: "1:394290136676:web:9848416d6de87eb2614171"
+  apiKey: 'AIzaSyAmrcw3LWh5vdrPE40gh2Uggxq3EG96Lys',
+  authDomain: 'film-library-registration.firebaseapp.com',
+  databaseURL:
+    'https://film-library-registration-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'film-library-registration',
+  storageBucket: 'film-library-registration.appspot.com',
+  messagingSenderId: '394290136676',
+  appId: '1:394290136676:web:9848416d6de87eb2614171',
 };
 
 // Initialize Firebase
@@ -25,9 +37,8 @@ const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
 const auth = getAuth();
 const user = auth.currentUser;
- const dt = new Date();
+const dt = new Date();
 
-  
 //Modal btns
 const signinBtn = document.querySelector('.signin-Btn');
 const loginBtn = document.querySelector('.login-Btn');
@@ -46,15 +57,14 @@ const nameInput = document.querySelector('.name-input');
 const formRegistr = document.querySelector('.registr');
 const formTitle = document.querySelector('.form-title');
 
-
 const myLibraryJs = document.querySelector('.my-library-js');
-myLibraryJs.style.display = 'none'
-logOut.style.display = 'none'
+myLibraryJs.style.display = 'none';
+logOut.style.display = 'none';
 
 //Проверка залогинен пользователь или нет
-function userIsLogin(){
-    const userIsLogin = localStorage.getItem("userIsLogin");
-    const userIsLoginParse = JSON.parse(userIsLogin);
+function userIsLogin() {
+  const userIsLogin = localStorage.getItem('userIsLogin');
+  const userIsLoginParse = JSON.parse(userIsLogin);
 
   if (userIsLoginParse) {
     signinBtn.style.display = 'none';
@@ -64,7 +74,7 @@ function userIsLogin(){
   }
 }
 
-userIsLogin()
+userIsLogin();
 
 function closeFormModal() {
   modal.classList.add('form-hidden');
@@ -74,7 +84,7 @@ function closeFormModal() {
   submitLoginBtn.style.display = 'none';
   document.removeEventListener('keydown', escModal);
   modal.removeEventListener('click', closeModalOutsideWindow);
-    refs.body.style.overflow = 'scroll'
+  refs.body.style.overflow = 'scroll';
 }
 
 modalFormBtnClose.addEventListener('click', closeFormModal);
@@ -92,114 +102,124 @@ function closeModalOutsideWindow(e) {
   closeFormModal();
 }
 
-
-
 //Открытие модалки(регистрация)
 signinBtn.addEventListener('click', e => {
   modal.classList.remove('form-hidden');
-      nameInput.style.display = 'block';
-    formRegistr.addEventListener('submit', signUpUser) 
+  nameInput.style.display = 'block';
+  formRegistr.addEventListener('submit', signUpUser);
   submitSignBtn.style.display = 'block';
-  formTitle.textContent = 'SIGN IN TO FILMOTEKA'
-  refs.body.style.overflow = 'hidden'
-    document.addEventListener('keydown', escModal);
+  formTitle.textContent = 'SIGN IN TO FILMOTEKA';
+  refs.body.style.overflow = 'hidden';
+  document.addEventListener('keydown', escModal);
   modal.addEventListener('click', closeModalOutsideWindow);
-})
+});
 
 //Регистрация пользователя
 function signUpUser(e) {
-     e.preventDefault()
-    const username = formRegistr.elements[0].value
-    const email = formRegistr.elements[1].value
-    const password = formRegistr.elements[2].value
+  e.preventDefault();
+  const username = formRegistr.elements[0].value;
+  const email = formRegistr.elements[1].value;
+  const password = formRegistr.elements[2].value;
 
-    modal.classList.add('form-hidden');
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;     
-            set(ref(database, 'users/' + user.uid), {
-                username: username,
-              queueList: '',
-                watchedList: '',
-                email: email,
-                last_login: dt,
-            })
-          handleLogIn()
-          removeEventListeners()
-            Notify.success('Congratulations, your account has been successfully created.',{
-    timeout: 1000,
-  },);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-                removeEventListeners()
-         
-          Notify.failure(errorMessage,{
-    timeout: 1000,
-  },);
-        })
-        e.target.reset()
+  modal.classList.add('form-hidden');
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      // Signed in
+      const user = userCredential.user;
+      set(ref(database, 'users/' + user.uid), {
+        username: username,
+        queueList: '',
+        watchedList: '',
+        email: email,
+        last_login: dt,
+      });
+      handleLogIn();
+      removeEventListeners();
+      Notify.success(
+        'Congratulations, your account has been successfully created.',
+        {
+          timeout: 1000,
+        }
+      );
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      removeEventListeners();
+
+      Notify.failure(errorMessage, {
+        timeout: 1000,
+      });
+    });
+  e.target.reset();
 }
 
 //Открытие модалки(логин)
 
 loginBtn.addEventListener('click', e => {
-    formRegistr.addEventListener('submit', logInUser); 
-  formTitle.textContent = 'LOG IN TO FILMOTEKA'
-    modal.classList.remove('form-hidden');
-    nameInput.style.display = 'none';
-    submitSignBtn.style.display = 'none';
+  formRegistr.addEventListener('submit', logInUser);
+  formTitle.textContent = 'LOG IN TO FILMOTEKA';
+  modal.classList.remove('form-hidden');
+  nameInput.style.display = 'none';
+  submitSignBtn.style.display = 'none';
   submitLoginBtn.style.display = 'block';
-  refs.body.style.overflow = 'hidden'
-    document.addEventListener('keydown', escModal);
+  refs.body.style.overflow = 'hidden';
+  document.addEventListener('keydown', escModal);
   modal.addEventListener('click', closeModalOutsideWindow);
-
-})
+});
 
 //Логинизация
 function logInUser(e) {
-    e.preventDefault()
-  const username = formRegistr.elements[0].value 
-    const email = formRegistr.elements[1].value
-    const password = formRegistr.elements[2].value
+  e.preventDefault();
+
+  const username = formRegistr.elements[0].value;
+  const email = formRegistr.elements[1].value;
+  const password = formRegistr.elements[2].value;
+
+  localStorage.setItem('userId', '');
 
   signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    const uid = user.uid;   
+    .then(userCredential => {
+      // Signed in
+      const user = userCredential.user;
+      const uid = user.uid;
 
-         update(ref(database, 'users/' + user.uid),{
-             last_login: dt,
-         })
-      handleLogIn()
-         removeEventListeners()
-        const dbRef = ref(getDatabase());
-get(child(dbRef, `users/${uid}`)).then((snapshot) => {
-  if (snapshot.exists()) {
-     Notify.success(`Hi, ${snapshot.val().username || 'Anonymus' }, you are logged in successfully.`,{
-    timeout: 1000,
-  },);
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-     removeEventListeners()
-  console.error(error);
-});
-    
+      localStorage.setItem('userId', uid);
 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
+      update(ref(database, 'users/' + user.uid), {
+        last_login: dt,
+      });
+      handleLogIn();
+      removeEventListeners();
+      const dbRef = ref(getDatabase());
+      get(child(dbRef, `users/${uid}`))
+        .then(snapshot => {
+          if (snapshot.exists()) {
+            Notify.success(
+              `Hi, ${
+                snapshot.val().username || 'Anonymus'
+              }, you are logged in successfully.`,
+              {
+                timeout: 1000,
+              }
+            );
+          } else {
+            console.log('No data available');
+          }
+        })
+        .catch(error => {
+          removeEventListeners();
+          console.error(error);
+        });
+    })
+    .catch(error => {
+      const errorCode = error.code;
       const errorMessage = error.message;
-    formRegistr.removeEventListener('submit', signUpUser);
+      formRegistr.removeEventListener('submit', signUpUser);
       Notify.failure(errorMessage, {
-    timeout: 1000,
-  },);
-  });   
+        timeout: 1000,
+      });
+    });
 }
 
 function handleLogIn() {
@@ -209,30 +229,29 @@ function handleLogIn() {
   logOut.style.display = 'block';
   myLibraryJs.style.display = 'block';
   submitLoginBtn.style.display = 'none';
-  localStorage.setItem("userIsLogin", "true");
-  refs.body.style.overflow = 'scroll'
+  localStorage.setItem('userIsLogin', 'true');
+  refs.body.style.overflow = 'scroll';
 }
 
 //Получение данных если пользоваетль залогинен
-onAuthStateChanged(auth, (user) => {
-
+onAuthStateChanged(auth, user => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid; 
-      const dbRef = ref(getDatabase());
-
-get(child(dbRef, `users/${uid}`)).then((snapshot) => {
-
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
-});
+    const uid = user.uid;
+    const dbRef = ref(getDatabase());
+    localStorage.setItem('userId', uid);
+    get(child(dbRef, `users/${uid}`))
+      .then(snapshot => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+        } else {
+          console.log('No data available');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
     // ...
   } else {
     // User is signed out
@@ -240,33 +259,35 @@ get(child(dbRef, `users/${uid}`)).then((snapshot) => {
   }
 });
 
-
 //Выйти из аккаунта
 logOut.addEventListener('click', e => {
-    signOut(auth).then(() => {
-  // Sign-out successful.
-           signinBtn.style.display = 'block';
-            loginBtn.style.display = 'block';
-        logOut.style.display = 'none'
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      signinBtn.style.display = 'block';
+      loginBtn.style.display = 'block';
+      logOut.style.display = 'none';
       myLibraryJs.style.display = 'none';
-      
-      localStorage.setItem("userIsLogin", "false");
-      switchToHome(e)
-      fetchTrendingFilms()
-      Notify.success('Successful logged out.',{
-    timeout: 1000,
-  },);
-}).catch((error) => {
-   const errorCode = error.code;
-      const errorMessage = error.message;
-       Notify.failure(errorMessage, {
-    timeout: 1000,
-  },);
-});
-})
 
+      localStorage.setItem('userId', '');
+
+      localStorage.setItem('userIsLogin', 'false');
+      switchToHome(e);
+      fetchTrendingFilms();
+      Notify.success('Successful logged out.', {
+        timeout: 1000,
+      });
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Notify.failure(errorMessage, {
+        timeout: 1000,
+      });
+    });
+});
 
 function removeEventListeners() {
-     formRegistr.removeEventListener('submit', signUpUser);
-    formRegistr.removeEventListener('submit', logInUser); 
+  formRegistr.removeEventListener('submit', signUpUser);
+  formRegistr.removeEventListener('submit', logInUser);
 }
