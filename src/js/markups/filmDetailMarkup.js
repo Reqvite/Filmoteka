a1a31569bf53a610d8daa1d0a9512bcf304df931
SerhiiSamara <ndaair@gmail.com>
2022-11-09 02:58:29 +0200

    Второй вариант

--- src/js/markups/filmDetailMarkup.js
+++ src/js/markups/filmDetailMarkup.js
@@ -1,6 +1,4 @@
-
-
-import img from '../../images/collection/csaff-no-poster.jpg'
+import img from '../../images/collection/csaff-no-poster.jpg';
 import viewTrailer from '../viewTrailer';
 
 import { onClickBtn } from '../myLibrary';
@@ -8,8 +6,7 @@ import { onClickAddToWatched } from '../addFilmToWatchedList-firebase';
 
 const modal = document.querySelector('.film-modal-content');
 
-export const createFilmDetailsMarkup = resp =>{
-
+export const createFilmDetailsMarkup = resp => {
   let {
     poster_path,
     original_title,
@@ -18,57 +15,62 @@ export const createFilmDetailsMarkup = resp =>{
     popularity,
     genres,
     overview,
-    id
+    id,
   } = resp.data;
- 
-            poster_path
-              ? poster_path = `https://www.themoviedb.org/t/p/original/${poster_path}`
-              : poster_path = img
-            vote_average
-              ? vote_average = vote_average.toFixed(1)
-              : (vote_average = '?');
-  
-  
+
+  poster_path
+    ? (poster_path = `https://www.themoviedb.org/t/p/original/${poster_path}`)
+    : (poster_path = img);
+  vote_average
+    ? (vote_average = vote_average.toFixed(1))
+    : (vote_average = '?');
+
   const markup = `<div class="film-details-wrapper">
-  <div><img class="modal-img" src="${poster_path}" alt="${original_title}" data-id="${id}"/></div>
-  <div class="film-details">
-    <h2 class="film-details__main-title">${original_title}</h2>
-    <ul class="details-list list">
-      <li class="details-list__item">
-        <p class="details-list_title">Vote / Votes</p>
-        <span class="details-list__information-1">${vote_average}</span>&nbsp/&nbsp<span class="details-list__information-2">${vote_count}</span>
-      </li>
-      <li class="details-list__item">
-        <p class="details-list_title">Popularity</p>
-        <span class="details-list__information-2">${popularity}</span>
-      </li>
-      <li class="details-list__item">
-        <p class="details-list_title">Original Title</p>
-        <span class="details-list__information-2 ">${original_title}</span>
-      </li>
-      <li class="details-list__item">
-        <p class="details-list_title">Genre</p>
-        <span class="details-list__information-2">${genres.map(el => {
+	<div>
+		<div class="thumb">
+			<img class="modal-img" src="${poster_path}" alt="${original_title}" data-id="${id}" />
+			<button class="trailer-button trailer-button--view button" type="button">
+				<span class="trailer-button__text trailer-button__text--play">View trailer</span>
+			</button>
+		</div>
+	</div>
+	<div class="film-details">
+		<h2 class="film-details__main-title">${original_title}</h2>
+		<ul class="details-list list">
+			<li class="details-list__item">
+				<p class="details-list_title">Vote / Votes</p>
+				<span class="details-list__information-1">${vote_average}</span>&nbsp/&nbsp<span
+					class="details-list__information-2">${vote_count}</span>
+			</li>
+			<li class="details-list__item">
+				<p class="details-list_title">Popularity</p>
+				<span class="details-list__information-2">${popularity}</span>
+			</li>
+			<li class="details-list__item">
+				<p class="details-list_title">Original Title</p>
+				<span class="details-list__information-2 ">${original_title}</span>
+			</li>
+			<li class="details-list__item">
+				<p class="details-list_title">Genre</p>
+				<span class="details-list__information-2">${genres.map(el => {
           return el.name;
         })}</span>
-      </li>
-    </ul>
-    <h3 class="film-details__secondary-title">About</h3>
-    <p class="film-details__about">${overview}</p>
-    <ul class="buttons-list list">
-  <li class="buttons-list__item">
-    <button class="main-button button" type="button" name="watched">Add to watched</button>
-  </li>
-  <li class="buttons-list__item"><button class="secondary-button button" type="button" name="queue">Add to queue</button></li>
-</ul>
-
-<button class="trailer-button button" type="button">
-<span class="trailer-button__text trailer-button__text--play">View trailer</span>
-</button>
-<div class="trailer"></div>
-</div>
-
-  </div>
+			</li>
+		</ul>
+		<h3 class="film-details__secondary-title">About</h3>
+		<p class="film-details__about">${overview}</p>
+		<ul class="buttons-list list">
+			<li class="buttons-list__item">
+				<button class="main-button button" type="button" name="watched">Add to watched</button>
+			</li>
+			<li class="buttons-list__item"><button class="secondary-button button" type="button" name="queue">Add to
+					queue</button></li>
+		</ul>
+		<div class="trailer"></div>
+		<button class="trailer-button trailer-button--close button is-hidden" type="button">
+				<span class="trailer-button__text trailer-button__text--stop">closed trailer</span>
+			</button>
+	</div>
 </div>`;
 
   modal.insertAdjacentHTML('beforeend', markup);
