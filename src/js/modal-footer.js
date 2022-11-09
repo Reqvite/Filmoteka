41b2e4dbb32b17ebe92dcd3fdfadeed39f456320
Darya-Viunyk <dar.viunyk@gmail.com>
2022-11-09 18:44:29 +0200

    change photo and without unnecessary scrolling

--- src/js/modal-footer.js
+++ src/js/modal-footer.js
@@ -5,8 +5,10 @@ import * as image3 from '../images/footer/Alexander-Kulyk.jpg';
 import * as image4 from '../images/footer/me-foto-1.jpg';
 import * as image5 from '../images/footer/Taras-Novitskyi.jpg';
 import * as image6 from '../images/footer/Yevhenii-Kavetskyi.jpg';
+import * as image7 from '../images/footer/img20.jpg';
 import * as image9 from '../images/footer/Inna-Sikora.jpg';
 import * as image8 from '../images/footer/Lina-Fomenko.jpg';
+import * as image10 from '../images/footer/270.jpg';
 import * as sprite from '../images/sprite.svg';
 
 (() => {
@@ -41,16 +43,16 @@ import * as sprite from '../images/sprite.svg';
   //////// cart////////
 
   const cart = [
-    {img: `${image}`, h3: `Scrum Master Yurii Perekrestnyi`, a:`https://github.com/Jodlei`, href: `https://uk.linkedin.com/`},
+    {img: `${image}`, h3: `Scrum Master Yurii Perekrestnyi`, a:`https://github.com/Jodlei`, href: `https://www.linkedin.com/in/yurii-perekrestnyi-998396256/`},
     {img: `${image}`, h3: `Team Lead Vladyslav Yachyn`, a:`https://github.com/Reqvite`, href: `https://uk.linkedin.com/`},
     {img: `${image3}`, h3: `Alexander Kulyk`, a:`https://github.com/alexander-kulyk`, href: `https://uk.linkedin.com/`},
-    {img: `${image4}`, h3: `Viunyk Daria`, a:`https://github.com/Darya-Viunyk`, href: `https://www.linkedin.com/in/darya-viunyk-50b386181/`},
+    {img: `${image4}`, h3: `Daria Viunyk`, a:`https://github.com/Darya-Viunyk`, href: `https://www.linkedin.com/in/darya-viunyk-50b386181/`},
     {img: `${image5}`, h3: `Taras Novitskyi`, a:`https://github.com/Taras-Novitskyi`, href: `https://uk.linkedin.com/`},
     { img: `${image6}`, h3: `Yevhenii Kavetskyi`, a: `https://github.com/eugeniusz57`, href: `https://www.linkedin.com/in/yevhenii-kavetskyi-423a34250/` },
-    { img: `${image}`, h3: `Sergii Samara`, a: `https://github.com/SerhiiSamara`, href: `https://uk.linkedin.com/` },
+    { img: `${image7}`, h3: `Sergii Samara`, a: `https://github.com/SerhiiSamara`, href: `https://www.linkedin.com/in/serhii-samara-73a0397a/` },
     { img: `${image8}`, h3: `Lina Fomenko`, a: `https://github.com/linafv`, href: `https://uk.linkedin.com/` },
     { img: `${image9}`, h3: `Inna Sikora`, a: `https://github.com/Inna2794`, href: `https://uk.linkedin.com/` },
-    { img: `${image}`, h3: `Vlad Lysenko`, a: `https://github.com/eugeniusz57`, href: `https://uk.linkedin.com/` }, 
+    { img: `${image10}`, h3: `Vladyslav Lysenko`, a: `https://github.com/VladyslavLysenko?tab=repositories`, href: `https://uk.linkedin.com/` }, 
   ];
   // DFilmoteka\src\images\sprite.svg:\archive\Goit\js\
   const paletteCarts = document.querySelector(`.js-palette`);
