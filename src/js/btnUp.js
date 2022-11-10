9199456798264f7558c9fb930135bc668b6ac8d3
linafv <ljh.amp.05@gmail.com>
2022-11-10 19:32:59 +0200

    fix the val of scroll option for a later btn appearance

--- src/js/btnUp.js
+++ src/js/btnUp.js
@@ -1,18 +1,20 @@
-import { refs } from "./refs/refs";
+import { refs } from './refs/refs';
 
 window.addEventListener('scroll', () => {
-    // определяем величину прокрутки
-    const scrollY = window.scrollY || document.documentElement.scrollTop;
-    // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-    scrollY > 400 ? refs.btnUp.classList.remove('is-hiden') : refs.btnUp.classList.add('is-hiden');
-  });
+  // определяем величину прокрутки
+  const scrollY = window.scrollY || document.documentElement.scrollTop;
+  // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+  scrollY > 2000
+    ? refs.btnUp.classList.remove('is-hiden')
+    : refs.btnUp.classList.add('is-hiden');
+});
 
-  refs.btnUp.addEventListener('click', onScrollUp);
-  
-  function onScrollUp() {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      });
-  }
+refs.btnUp.addEventListener('click', onScrollUp);
+
+function onScrollUp() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+}
