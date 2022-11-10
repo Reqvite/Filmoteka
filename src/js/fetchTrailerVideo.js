3d26825670e4c0a0e7b648ec224d3a1813b9171c
SerhiiSamara <ndaair@gmail.com>
2022-11-10 17:11:35 +0200

    Подчистил. Исправил сообщение Error

--- src/js/fetchTrailerVideo.js
+++ src/js/fetchTrailerVideo.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 const API_KEY = 'e145377b3a98d62607e7dc90339d279b';
 
 export default async function fetchTrailerVideo(id) {
-  return axios({
-    method: 'get',
-    url: `https://api.themoviedb.org/3/movie/${id}/videos?${API_KEY}b&language=en-US`,
+  const options = {
     params: {
       api_key: API_KEY,
       language: 'en-US',
     },
-  });
+  };
+
+  return axios.get(`${BASE_URL}${id}/videos`, options);
 }
