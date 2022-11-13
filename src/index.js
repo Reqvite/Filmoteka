99af1294fc269614d12352e89898b9113b085eb3
Reqvite <reqvite2233@gmail.com>
2022-11-13 15:20:22 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import './sass/index.scss';
 
-//markups
-
-
-
-import './js/modals/modal-footer';
-
 import './js/service/addFilmsAndrenderGalery';
 
-
-
-import './js/markups/filmDetailMarkup';
-
-import './js/modals/openFilmModal';
-
-import './js/service/viewTrailer';
-
-import './js/componets/pagination';
-
-import './js/library/myLibrary';
-
 import './js/componets/toggleTheme';
-
-import './js/service/changeHeaderPageHome-Mylibrary';
 
 import './js/library/addFilmToWatchedList-firebase';
 
 import "./js/componets/btnUp";
+
+import './js/modals/modal-footer';
